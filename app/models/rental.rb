14a1fea8e335f105a0d82f017791db8ef85faafb
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-02-14 19:18:26 -0500

    first two deliverables

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,26 @@
 class Rental < ActiveRecord::Base
     belongs_to :client 
     belongs_to :vhs
+    
+    
+    def due_date
+        created_at + 7.days
+    end
+   
+    def past_due_date?
+        self.updated_at < self.due_date
+    end
+
+    def self.past_due_date
+        binding.pry
+        self.select {|rental| rental.past_due_date?}
+    end
+
 end
+
+
+# Rental.past_due_date - 
+# returns a list of all the rentals - iterate over that list to return - if updated_at - created_at > 7days return that array
+# - all that are past due date, 
+# - currently rented 
+# - or rented in the past
